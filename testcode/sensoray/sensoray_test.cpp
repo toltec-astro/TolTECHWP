2b09061111ee66fa74fc85d5ed25b3c6f283b402
Eric Van Camp <ericvancamp2019@u.northwestern.edu>
2018-08-09 15:59:41 -0500

    fixed for consistent tabs

--- testcode/sensoray/sensoray_test.cpp
+++ testcode/sensoray/sensoray_test.cpp
@@ -8,18 +8,18 @@
 #include "stdio.h"
 #include "826api.h"
 int main() {
-int id;     // board ID
-int flags = S826_SystemOpen();
-if (flags < 0)  {
-  printf("S826_systemOpen returned error code %d", flags);
-}  else if (flags == 0)  {
-  printf("No boards were detected");
-}  else  {
-  printf("boards were detected with these IDs:");
-  for (id = 0; id < 16; id++) {
-    if (flags & (1 << id))  {
-      printf(" %d", id);
+  int id;     // board ID
+  int flags = S826_SystemOpen();
+  if (flags < 0)  {
+    printf("S826_systemOpen returned error code %d", flags);
+  }  else if (flags == 0)  {
+    printf("No boards were detected");
+  }  else  {
+    printf("boards were detected with these IDs:");
+    for (id = 0; id < 16; id++) {
+      if (flags & (1 << id))  {
+        printf(" %d", id);
+      }
     }
   }
-}
 }
