087fef3783f11d511e5920a86c4a51f5f8f1916a
Dennis Lee <dennislee2023@u.northwestern.edu>
2018-11-12 16:32:35 -0600

    reorder

--- testcode/sensors/housekeeping.c
+++ testcode/sensors/housekeeping.c
@@ -11,6 +11,19 @@
 
 // TODO: convert individual configurations into a struct
 
+static int AdcReadSlot(uint board, uint slot, int *slotdata)
+{
+    int adcbuf[16];
+    uint slotlist = 1 << slot;                                                      // set up the adc slot list; we are only interested in one slot
+    int errcode = S826_AdcRead(board, adcbuf, NULL, &slotlist, 1000);                     // wait for data to arrive on the slot of interest (in response to adc hardware trigger)
+    if ((unsigned int) errcode == S826_ERR_MISSEDTRIG)      // this application doesn't care about adc missed triggers
+        errcode = S826_ERR_OK;
+    *slotdata = adcbuf[slot] & 0xFFFF;                                                       // copy adc data to slotdata buffer
+
+    return errcode;
+}
+
+
 void AdcHandler(void)
 {
     // reads in the data.
@@ -56,17 +69,6 @@ void AdcHandler(void)
 
 }
 
-static int AdcReadSlot(uint board, uint slot, int *slotdata)
-{
-    int adcbuf[16];
-    uint slotlist = 1 << slot;                                                      // set up the adc slot list; we are only interested in one slot
-    int errcode = S826_AdcRead(board, adcbuf, NULL, &slotlist, 1000);                     // wait for data to arrive on the slot of interest (in response to adc hardware trigger)
-    if ((unsigned int) errcode == S826_ERR_MISSEDTRIG)      // this application doesn't care about adc missed triggers
-        errcode = S826_ERR_OK;
-    *slotdata = adcbuf[slot] & 0xFFFF;                                                       // copy adc data to slotdata buffer
-
-    return errcode;
-}
 
 char t[200];
 int main(int argc, char **argv){
