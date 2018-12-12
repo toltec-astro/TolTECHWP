ee1d0b871b3fb3cf00fc78c185889c763849cff1
Dennis Lee <dennislee2023@u.northwestern.edu>
2018-12-12 10:47:00 -0600

    remove incorrect path

--- readout/readout.c
+++ readout/readout.c
@@ -1,5 +1,5 @@
 #include "../vend/ini/ini.h"
-#include "../vend/ini/sdk_826_linux_3.3.11/driver/826api.h"
+#include "../vend/sdk_826_linux_3.3.11/driver/826api.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
