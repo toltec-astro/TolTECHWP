3d8c99cffef618dc975964c56336cd54a39d30e7
Dennis Lee <dennislee2023@u.northwestern.edu>
2018-12-12 16:03:36 -0600

    reordering of functions

--- readout/readout.c
+++ readout/readout.c
@@ -10,58 +10,6 @@
 #include <signal.h>
 
 //ini_free(config);
-
-int main(int argc, char **argv){
-    // signal handler
-    signal(SIGINT, SystemCloseHandler);
-
-    // load config file
-    LoadConfig();
-    
-    if( argc < 3 ) {
-        printf("Use: readout datadtime sleepdtime duration\n");
-        printf("     datadtime = intervals how often data is read (us)\n");
-        printf("     sleepdtime = how long computer sleeps between reads (us)\n");
-        printf("     duration = how long experiment is (seconds)\n");
-        exit(0);
-    }    
-    // Variables
-    int i;
-    int board = 0;
-    int countquad = 0; // quadrature counter
-    int countime = 2; // timer counter
-    int countpps = 1; // pps counter
-    struct timespec treq;
-    time_t rawtime,startime;
-    char s[2000000],t[200];
-    char fname[100];
-    FILE *outf;
-    // Variables to Read all snapshots while it's moving
-    int lastcount = 0; // last value which was read
-    int dcount = 1; // diff current - previous value
-    int loopcount = 0; // number of loops with delays
-    int errcount = 0; // number of overflow errors
-    int sampcount = 0; // number of samples in this readout session
-    uint counts[1000], tstamp[1000], reason[1000];
-    //// Preparation
-    // Set timer counter interval: Determines how often count data is stored.
-    int datausec = atoi(argv[1]); // Micro seconds
-    // Set computer sleep interval
-    int sleepusec = atoi(argv[2]);
-    treq.tv_sec = 0;
-    treq.tv_nsec = sleepusec * 1000;
-    // Set duration
-    int duration = atoi(argv[3]); // Seconds
-
-    // Open the 826 API ---------------
-    // int flags = S826_SystemOpen();
-    // printf("S826_SystemOpen returned error code %d\n", flags);
-    SystemOpenHandler();
-
-    // close the 826 API
-    SystemCloseHandler();
-}
-
 void SystemCloseHandler(int sig)
 {
     signal(sig, SIG_IGN);
@@ -145,3 +93,55 @@ void ConfigurePulsePerSecondCounter(int board, int countpps)
     if (flags < 0)
         printf("PPS Counter returned error code %d\n", flags);   
 }
+int main(int argc, char **argv){
+    // signal handler
+    signal(SIGINT, SystemCloseHandler);
+
+    // load config file
+    LoadConfig();
+    
+    if( argc < 3 ) {
+        printf("Use: readout datadtime sleepdtime duration\n");
+        printf("     datadtime = intervals how often data is read (us)\n");
+        printf("     sleepdtime = how long computer sleeps between reads (us)\n");
+        printf("     duration = how long experiment is (seconds)\n");
+        exit(0);
+    }    
+    // Variables
+    int i;
+    int board = 0;
+    int countquad = 0; // quadrature counter
+    int countime = 2; // timer counter
+    int countpps = 1; // pps counter
+    struct timespec treq;
+    time_t rawtime,startime;
+    char s[2000000],t[200];
+    char fname[100];
+    FILE *outf;
+    // Variables to Read all snapshots while it's moving
+    int lastcount = 0; // last value which was read
+    int dcount = 1; // diff current - previous value
+    int loopcount = 0; // number of loops with delays
+    int errcount = 0; // number of overflow errors
+    int sampcount = 0; // number of samples in this readout session
+    uint counts[1000], tstamp[1000], reason[1000];
+    //// Preparation
+    // Set timer counter interval: Determines how often count data is stored.
+    int datausec = atoi(argv[1]); // Micro seconds
+    // Set computer sleep interval
+    int sleepusec = atoi(argv[2]);
+    treq.tv_sec = 0;
+    treq.tv_nsec = sleepusec * 1000;
+    // Set duration
+    int duration = atoi(argv[3]); // Seconds
+
+    // Open the 826 API ---------------
+    // int flags = S826_SystemOpen();
+    // printf("S826_SystemOpen returned error code %d\n", flags);
+    SystemOpenHandler();
+
+    // close the 826 API
+    SystemCloseHandler();
+}
+
+
