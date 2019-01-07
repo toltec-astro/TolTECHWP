83539068ae50de2cedafb3807db076041cc480d3
Dennis Lee <dennislee2023@u.northwestern.edu>
2019-01-07 11:15:33 -0600

    remove typo

--- readout/readout.c
+++ readout/readout.c
@@ -352,7 +352,7 @@ int main(int argc, char **argv){
     while(rawtime - starttime < duration){
         
         clock_gettime(CLOCK_MONOTONIC_RAW, &curtime);
-        delta_us = = (curtime.tv_sec - quadlastreadtime.tv_sec) * 1000000000 + (curtime.tv_nsec - quadlastreadtime.tv_nsec);
+        delta_us = (curtime.tv_sec - quadlastreadtime.tv_sec) * 1000000000 + (curtime.tv_nsec - quadlastreadtime.tv_nsec);
         if (quadlastreadtime > (1000000000 * atof(quad_intervals))){
             ReadQuadSnapshot();
             clock_gettime(CLOCK_MONOTONIC_RAW, &quadlastreadtime);
