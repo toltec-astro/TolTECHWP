349d739bb90859e601083bc08a6c68faa26e4073
Dennis Lee <dennislee2023@u.northwestern.edu>
2019-01-14 14:43:32 -0600

    only read sensors

--- readout/readout.c
+++ readout/readout.c
@@ -283,7 +283,7 @@ void ReadSensorSnapshot(void)
 
     int errcode;     // errcode 
     int slotval[16]; // buffer must be sized for 16 slots
-    errcode = S826_AdcRead(0, slotval, NULL, &slotlist, 10);//S826_WAIT_INFINITE); 
+    errcode = S826_AdcRead(0, slotval, NULL, &slotlist, S826_WAIT_INFINITE); 
     if (errcode != S826_ERR_OK)
         printf("ADC Read Failed. %d \n", errcode);
 
@@ -396,7 +396,7 @@ int main(int argc, char **argv){
         //printf("run quad:");
         if (delta_us > (1000000000 * atof(quad_intervals))){
             printf("quad yes\n");
-            ReadQuadSnapshot(board, atoi(countquad), tstart);
+            //ReadQuadSnapshot(board, atoi(countquad), tstart);
             // update last read time
             clock_gettime(CLOCK_MONOTONIC_RAW, &quadlastreadtime);
         }
@@ -413,7 +413,7 @@ int main(int argc, char **argv){
         //printf("run pps:");
         if (delta_us > (1000000000 * atof(pps_intervals))){
             printf("pps yes\n");
-            ReadPPSSnapshot(board, atoi(countpps), tstart);
+            //ReadPPSSnapshot(board, atoi(countpps), tstart);
             // update last read time
             priority_flag = 1;
             clock_gettime(CLOCK_MONOTONIC_RAW, &ppslastreadtime);
