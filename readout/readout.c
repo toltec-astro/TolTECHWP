e8b32e5d275159c0b3df3ea2a00b0f4000776105
Dennis Lee <dennislee2023@u.northwestern.edu>
2019-01-18 16:17:23 -0600

    reverting

--- readout/readout.c
+++ readout/readout.c
@@ -3,9 +3,9 @@
 #include "../vend/ini/ini.h"
 #include "../vend/sdk_826_linux_3.3.11/demo/826api.h"
 
-#include <time.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 #include <string.h>
 #include <stdlib.h> 
 #include <signal.h>
@@ -226,6 +226,7 @@ void ConfigurePulsePerSecondCounter(int board, ini_t *config)
 
 void ReadPPSSnapshot(int board, int countpps, uint tstart)
 {
+    //fprintf(stdout, "PPS. \n");
     int errcode;
     int sampcount = 0;
     uint counts[1000], tstamp[1000], reason[1000];
