f2d0623194544bfc5aecbbcfe25e4479fcdc2946
Dennis Lee <dennislee2023@u.northwestern.edu>
2019-04-03 08:58:08 -0500

    compile statement

--- readout/readout.c
+++ readout/readout.c
@@ -176,3 +176,8 @@ int main(int argc, char **argv){
         
 
 }
+
+/*
+gcc -D_LINUX -Wall -Wextra -DOSTYPE_LINUX -c -no-pie readout.c ../vend/ini/ini.c
+gcc -D_LINUX readout.o ini.o -no-pie -o readout -L ../vend/sdk_826_linux_3.3.11/demo -l826_64 -lpthread
+*/
