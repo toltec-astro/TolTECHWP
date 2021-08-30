4c8bf9aa27ccd3d19d26097ba3479605fb3fc35b
Eric Van Camp <ericvancamp2019@u.northwestern.edu>
2021-08-30 16:38:49 -0500

    Update galilagent, add RS to galil conf

--- controller/galilagent.py
+++ controller/galilagent.py
@@ -175,7 +175,9 @@ class GalilAgent(AgentParent):
                     retmsg = 'Already closed'
             # configure
             elif 'conf' in task.lower():
-                retmsg = self.command(self.config['galil']['confcomm'])
+                retmsg = self.command('RS')
+                time.sleep(0.5)
+                retmsg += self.command(self.config['galil']['confcomm'])
             # initialize
             elif 'init' in task.lower():
                 retmsg = self.command(self.config['galil']['initcomm'])
